7779381e8de7ebde770fed8c5970e94c2136a3f4
mohanbomrel <mohan.bomrel@deerwalk.edu.np>
2015-03-23 16:23:03 +0545

    Second Commit
    Made some changes

--- Book.cpp
+++ Book.cpp
@@ -1,6 +1,5 @@
-//#include <iostream>
+#include <iostream>
 #include <string>
-//#include "Author.h"
 #include "Book.h"
 
 using namespace std;
--- Book.h
+++ Book.h
@@ -23,7 +23,6 @@ public:
     void setQuantity(int );
     int getQuantity();
     string toString();
-   // string getAuthorName();
 
 
 };
